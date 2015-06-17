8196681ad8224d49cf7be4d323e63f11ceea7066
fumikito <guy@hametuha.com>
2015-06-17 17:50:53 +0900

    Facebook SDKを変更

--- header-meta.php
+++ header-meta.php
@@ -16,10 +16,19 @@
 </head>
 <body <?php body_class() ?>>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1&appId=196054397143922";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '196054397143922',
+			xfbml      : true,
+			version    : 'v2.3'
+		});
+	};
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/ja_JP/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
