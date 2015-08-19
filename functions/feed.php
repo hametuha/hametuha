14da7368a34d1e63ee522b4c9eac300217076eb1
fumikito <guy@hametuha.com>
2015-08-19 18:45:22 +0900

    KDPのリストを吐き出せるよう変更

--- functions/feed.php
+++ functions/feed.php
@@ -2,6 +2,8 @@
 
 /**
  * RSS2.0にメディアを追加する
+ *
+ * @see http://www.rssboard.org/media-rss
  */
 add_action( 'rss2_ns', function () {
 	echo 'xmlns:media="http://search.yahoo.com/mrss/"';
@@ -9,18 +11,31 @@ add_action( 'rss2_ns', function () {
 
 
 /**
- * サムネイルをメディアタグに書き出す
+ * RSSに追加情報をくっつける
  */
 add_action( 'rss2_item', function () {
+	$series = \Hametuha\Model\Series::get_instance();
+	// サムネイル
 	if ( has_post_thumbnail() ) {
 		$src   = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0];
 		$title = get_the_title_rss();
-		echo <<<EOS
+		echo <<<XML
 		<media:thumbnail url="{$src}" />
 		<media:content url="{$src}" medium="image">
 			<media:title type="html">{$title}</media:title>
 		</media:content>
-
-EOS;
+XML;
+	}
+	if ( 'series' == get_post_type() ) {
+		//  KDP ready
+		if ( 2 === $series->get_status( get_the_ID() ) ) {
+			$asin     = $series->get_asin( get_the_ID() );
+			$subtitle = esc_html( $series->get_subtitle( get_the_ID() ) );
+			echo <<<XML
+				<category>{$subtitle}</category>
+				<dc:identifier>{$asin}</dc:identifier>
+				<dc:relation>http://www.amazon.co.jp/dp/{$asin}/?t=hametuha-22</dc:relation>
+XML;
+		}
 	}
 } );
