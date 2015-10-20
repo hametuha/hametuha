d7edcff63cd59b61c8a400c2992dc3f43d945263
fumikito <guy@hametuha.com>
2015-10-20 11:53:10 +0900

    Facebookのピクセルタグを書き込み https://www.facebook.com/help/218844828315224

--- functions/analytics.php
+++ functions/analytics.php
@@ -64,6 +64,29 @@ function ga_record_cookie($page){
 	}
 }
 
+
+/**
+ * Facebookのピクセルタグを書き込む
+ */
+add_action('wp_head', function(){
+	echo <<<HTML
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+fbq('init', '956989844374988');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=956989844374988&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+HTML;
+}, 100);
+
+
 /**
  * セッションに書き込みがあったらGAに記録する
  */
