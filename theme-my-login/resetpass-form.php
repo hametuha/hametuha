7227141aac0f0c86a9a27cc23b99bca52233ad03
fumikito <guy@hametuha.com>
2016-02-13 22:17:58 +0900

    Fix password rest form.

--- theme-my-login/resetpass-form.php
+++ theme-my-login/resetpass-form.php
@@ -9,30 +9,36 @@ Theme My Login will always look in your theme's directory first, before using th
 		新しいパスワードを入力してください。
 	</p>
 	<?php $template->the_errors(); ?>
-	<form name="resetpasswordform" id="resetpasswordform<?php $template->the_instance(); ?>" action="<?php $template->the_action_url( 'resetpass' ); ?>" method="post">
+	<form name="resetpasswordform" id="resetpasswordform<?php $template->the_instance(); ?>" action="<?php $template->the_action_url( 'resetpass', 'login_post' ); ?>" method="post">
 
-        <input type="hidden" name="key" value="<?php $template->the_posted_value( 'key' ); ?>" />
-        <input type="hidden" name="login" id="user_login" value="<?php $template->the_posted_value( 'login' ); ?>" />
-        <input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
+		<input type="hidden" id="user_login" value="<?php echo esc_attr( $GLOBALS['rp_login'] ); ?>" autocomplete="off" />
+		<input type="hidden" name="rp_key" value="<?php echo esc_attr( $GLOBALS['rp_key'] ); ?>" />
+		<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
         <input type="hidden" name="action" value="resetpass" />
 
-        <div class="form-group">
-            <label for="pass1<?php $template->the_instance(); ?>">新しいパスワード</label>
-            <input autocomplete="off" name="pass1" id="pass1<?php $template->the_instance(); ?>" class="form-control" value="" type="password" autocomplete="off" />
+        <div class="form-group wp-pwd">
+            <label for="pass1">新しいパスワード</label>
+            <input autocomplete="off" name="pass1" id="pass1" data-reveal="1" data-pw="<?php echo esc_attr( wp_generate_password( 16 ) ); ?>" class="form-control" value="" type="text" autocomplete="off" />
         </div>
         <div class="form-group">
-            <label for="pass2<?php $template->the_instance(); ?>">パスワード確認</label>
-            <input autocomplete="off" name="pass2" id="pass2<?php $template->the_instance(); ?>" class="form-control" value="" type="password" autocomplete="off" />
+            <label for="pass2">パスワード確認</label>
+            <input autocomplete="off" name="pass2" id="pass2" class="form-control" value="" type="password" autocomplete="off" />
         </div>
 
 		<div id="pass-strength-result" class="hide-if-no-js"><?php _e( 'Strength indicator', 'theme-my-login' ); ?></div>
 
-		<p class="help-block"><?php _e( 'Hint: The password should be at least seven characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).', 'theme-my-login'); ?></p>
+		<p class="help-block"><?= wp_get_password_hint() ?></p>
+
+		<?php do_action('resetpassword_form'); ?>
 
 		<p>
 			<input class="btn btn-primary btn-block" type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="パスワード登録" />
 		</p>
 
 	</form>
-	<?php $template->the_action_links( array( 'lostpassword' => false ) ); ?>
+	<?php $template->the_action_links( [
+		'login' => false,
+	    'register' => false,
+	    'lostpassword' => false,
+	] ); ?>
 </div>
