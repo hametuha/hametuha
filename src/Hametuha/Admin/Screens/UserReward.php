d1b875313d81dc57f4212913dd79670d9e92b29a
fumikito <guy@hametuha.com>
2016-09-27 13:13:41 +0900

    close #25 タスクを個別に登録できるようにした

--- src/Hametuha/Admin/Screens/UserReward.php
+++ src/Hametuha/Admin/Screens/UserReward.php
@@ -31,6 +31,7 @@ class UserReward extends Screen {
 	 * Executed on admin_init
 	 */
 	public function adminInit() {
+		add_action( 'wp_ajax_hametuha_user_reward', [ $this, 'ajax_user_reward' ] );
 	}
 
 	/**
@@ -38,6 +39,46 @@ class UserReward extends Screen {
 	 */
 	protected function enqueueScript() {
 
+	}
+
+	/**
+	 * Save user data
+	 */
+	public function ajax_user_reward() {
+		try {
+			if ( ! $this->input->verify_nonce( 'add_user_reward' ) || ! current_user_can( 'administrator' ) ) {
+				throw new \Exception( 'あなたには権限がありません。', 401 );
+			}
+			$user_id = $this->input->post( 'user_id' );
+			if ( ! ( $user = get_userdata( $user_id ) ) ) {
+				throw new \Exception( '該当するユーザーは存在しません。', 404 );
+			}
+			if ( ! ( $created = $this->input->post( 'created' ) ) ) {
+				$created = current_time( 'mysql' );
+			}
+			$price = $this->input->post( 'price' );
+			if ( ! $price || ! is_numeric( $price ) ) {
+				throw new \Exception( sprintf( '金額が不正です: %s', $price ), 500 );
+			}
+			$vat_excluded = (bool) $this->input->post( 'vat' );
+			$unit = max( 1, $this->input->post( 'unit' ) );
+			if ( ! ( $desc = $this->input->post( 'description' ) ) ) {
+				throw new \Exception( '適用が入力されていません。', 500 );
+			}
+			if ( ! UserSales::get_instance()->add( $user_id, 'task', $price, $unit, $desc, ! $vat_excluded, true, 0, $created ) ) {
+				throw new \Exception( 'データの保存に失敗しました。', 500 );
+			}
+			wp_redirect( admin_url( 'users.php?page=hametuha-user-reward' ) );
+			exit;
+		} catch ( \Exception $e ) {
+			add_filter( 'wp_die_ajax_handler', function(){
+				return '_default_wp_die_handler';
+			} );
+			wp_die( $e->getMessage(), get_status_header_desc( $e->getCode() ), [
+				'response'  => $e->getCode(),
+			    'back_link' => true,
+			] );
+		}
 	}
 
 	/**
@@ -59,6 +100,63 @@ HTML;
 		ob_end_clean();
 		echo $content;
 		echo '</form>';
+		if ( current_user_can( 'administrator' ) ) :
+			?>
+			<hr />
+			<form id="user-reward-add-form" action="<?= admin_url( 'admin-ajax.php' ) ?>" method="post">
+				<input type="hidden" name="action" value="hametuha_user_reward" />
+				<?php wp_nonce_field( 'add_user_reward' ) ?>
+				<h2>ユーザーの報酬を追加</h2>
+				<table class="form-table">
+					<tr>
+						<th><label for="user_id">ユーザーID</label></th>
+						<td>
+							<input class="regular-text" type="number" name="user_id" id="user_id" value="" />
+						</td>
+					</tr>
+					<tr>
+						<th><label for="description">適用</label></th>
+						<td>
+							<input class="regular-text" type="text" name="description" id="description" value="" />
+						</td>
+					</tr>
+					<tr>
+						<th><label for="price">金額</label></th>
+						<td>
+							<input class="regular-text" type="number" name="price" id="price" value="" />
+						</td>
+					</tr>
+					<tr>
+						<th><label for="unit">数量</label></th>
+						<td>
+							<input class="regular-text" type="number" name="unit" id="unit" value="1" />
+						</td>
+					</tr>
+					<tr>
+						<th>消費税</th>
+						<td>
+							<label>
+								<input type="radio" name="vat" value="0" checked />
+								内税
+							</label>
+							<br />
+							<label>
+								<input type="radio" name="vat" value="1" />
+								外税
+							</label>
+						</td>
+					</tr>
+					<tr>
+						<th><label for="created">日付</label></th>
+						<td>
+							<input class="regular-text" type="text" name="created" id="created" value="" />
+						</td>
+					</tr>
+				</table>
+				<?php submit_button( '追加' ) ?>
+			</form>
+		<?php
+		endif;
 	}
 
 
