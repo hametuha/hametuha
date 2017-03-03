51d163492c0f65763ad4bf92713b6d78fbc940cb
fumikito <guy@hametuha.com>
2017-03-03 15:29:18 +0900

    Fix search results on FAQ.

--- functions/display.php
+++ functions/display.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * ページテンプレートを差し替え
- */
-add_filter( 'template_include', function( $path ) {
-	if ( is_singular( 'page' ) && ! is_home() && 'index.php' == basename( $path ) ) {
-		$path = get_template_directory().'/single.php';
-	}
-	return $path;
-} );
 
 
 /**
---
+++ hooks/template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * ページテンプレートを差し替え
+ */
+add_filter( 'template_include', function ( $path ) {
+	if ( is_singular( 'page' ) && ! is_home() && 'index.php' == basename( $path ) ) {
+		$path = get_template_directory() . '/single.php';
+	} elseif ( is_search() && is_post_type_archive( 'faq' ) ) {
+		$path = get_template_directory() . '/index.php';
+	}
+
+	return $path;
+} );
