838b524eb4ac9cf08796b456efadae598304200b
fumikito <guy@hametuha.com>
2018-08-02 02:11:01 +0900

    サイドバーをキャッシュ

--- sidebar-faq.php
+++ sidebar-faq.php
@@ -1,5 +1,15 @@
 <div class="col-xs-12 col-sm-3" id="sidebar" role="navigation">
 
-	<?php dynamic_sidebar( 'faq-sidebar' ); ?>
+	<?php
+    $sidebar = get_transient( 'faq_sidebar' );
+    if ( false === $sidebar ) {
+        ob_start();
+        dynamic_sidebar( 'faq-sidebar' );
+        $sidebar = ob_get_contents();
+        ob_end_clean();
+        set_transient( 'faq-sidebar', $sidebar, 60 * 10 );
+    }
+    echo $sidebar;
+    ?>
 
 </div><!-- //#sidebar -->
