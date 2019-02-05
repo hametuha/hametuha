82f41ca0ba637b4c71f2b15a0cba1c0959a981aa
fumikito <guy@hametuha.com>
2019-02-05 19:45:49 +0900

    Allow user to anonymous comment.

--- hooks/thread.php
+++ hooks/thread.php
@@ -43,7 +43,29 @@ add_action( 'hamethread_after_thread_form', function( $args, $default ) {
             気づいてほしい場合は通知を送ってください。
 		</small>
 	</div>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="1" name="post_as_anonymous" id="post_as_anonymous" aria-describedby="post_as_anonymous_description" />
+        <label class="form-check-label" for="post_as_anonymous">
+            匿名で投稿する
+        </label>
+        <small id="post_as_anonymous_description" class="form-text text-muted">
+            匿名で投稿すると、あとから編集できません。
+        </small>
+    </div>
 	<?php
+}, 10, 2 );
+
+/**
+ * Validation for private && anonymous
+ */
+add_filter( 'hamethread_new_thread_validation', function( WP_Error $error, WP_REST_Request $request ) {
+    if ( $request->get_param( 'is_private' ) && $request->get_param( 'post_as_anonymous' ) ) {
+        $error->add( 'hidden_thread', '匿名かつ非公開だと誰も見られないスレッドになってしまいます。', [
+            'response' => 400,
+            'status'   => 400,
+        ] );
+    }
+    return $error;
 }, 10, 2 );
 
 /**
@@ -58,26 +80,135 @@ add_filter( 'hamethread_new_thread_post_params', function( $args ) {
             return is_numeric( $var );
         },
     ];
+    $args['post_as_anonymous'] = [
+		'type'        => 'int',
+		'description' => 'Flag to detect post as anonymous.',
+		'default'     => 0,
+		'validate_callback' => function( $var ) {
+			return is_numeric( $var );
+		},
+    ];
     return $args;
 } );
 
 /**
- * Send notification to editor.
- *
+ * Allow anonymous post.
+ */
+add_filter( 'hamethread_new_thread_post_arg', function( array $args, WP_REST_Request $request ) {
+    if ( $request->get_param( 'post_as_anonymous' ) && ( $anonymous = get_anonymous_user() ) ) {
+        $args['post_author'] = $anonymous->ID;
+    }
+    return $args;
+}, 10, 2 );
+
+/**
+ * Remove current user from watch list.
+ */
+add_filter( 'hamethread_default_subscribers', function( array $subscribers, $post_id, $user_id, WP_REST_Request $request ) {
+    if ( $request->get_param( 'post_as_anonymous') ) {
+        $filtered = [];
+        foreach ( $subscribers as $subscriber ) {
+            if ( $subscriber != $user_id ) {
+                $filtered[] = $subscriber;
+            }
+        }
+        $subscribers = $filtered;
+    }
+    return $subscribers;
+}, 10, 4 );
+
+/**
+ * Anonymous and notification staff
  */
 add_action( 'hamethread_new_thread_inserted', function( $post_id, WP_REST_Request $request ) {
-    if ( ! $request->get_param( 'notify_to_editor' ) ) {
+    $post = get_post( $post_id );
+    // If anonymous, mark ID.
+	if ( $request->get_param( 'post_as_anonymous' ) && ( $anonymous = get_anonymous_user() ) && $post->post_author == $anonymous->ID ) {
+        update_post_meta( $post->ID, '_anonymous_author', get_current_user_id() );
+    }
+    // Send slack notification.
+	if ( $request->get_param( 'notify_to_editor' ) ) {
+		do_action( 'hameslack', '掲示板に書き込みがありました。', [ [
+			'fallback' => get_the_title( $post ),
+			'title' => get_the_title( $post ),
+			'title_link' => get_permalink( $post ),
+			'author_link' => get_permalink( $post ),
+			'author_name' => get_the_author_meta( 'display_name', $post->post_author ),
+			'color' => '#E80000',
+			'text' => '編集者に見て欲しいそうです。',
+		] ], '#admin' );
+	}
+}, 10, 2 );
+
+/**
+ * Allow anonymous comment.
+ */
+add_filter( 'hamethread_new_comment_rest_args', function( $args, $http_method ) {
+    if ( 'POST' === $http_method ) {
+        $args['post_as_anonymous'] = [
+			'type'        => 'int',
+			'description' => 'Flag to detect post as anonymous.',
+			'default'     => 0,
+			'validate_callback' => function( $var ) {
+				return is_numeric( $var );
+			},
+        ];
+    }
+    return $args;
+}, 10, 2 );
+
+/**
+ * Display anonymous comment toggle.
+ */
+add_action( 'hamethread_after_comment_form', function( $args, $comment = null ) {
+    if ( $comment ) {
         return;
     }
-    $post = get_post( $post_id );
-    // Send slack notification.
-    do_action( 'hameslack', '掲示板に書き込みがありました。', [ [
-		'fallback'    => get_the_title( $post ),
-		'title'       => get_the_title( $post ),
-		'title_link'  => get_permalink( $post ),
-		'author_link' => get_permalink( $post ),
-		'author_name' => get_the_author_meta( 'display_name', $post->post_author ),
-		'color'       => '#E80000',
-        'text'        => '編集者に見て欲しいそうです。',
-    ] ], '#admin' );
+    ?>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="1" name="post_as_anonymous" id="post_as_anonymous" aria-describedby="post_as_anonymous_description" />
+        <label class="form-check-label" for="post_as_anonymous">
+            匿名でコメントする
+        </label>
+        <small id="post_as_anonymous_description" class="form-text text-muted">
+            匿名でコメントすると、あとから編集できません。
+        </small>
+    </div>
+    <?php
+}, 10, 2 );
+
+/**
+ * Post comment.
+ */
+add_filter( 'hamethread_new_comment_params', function( array $params, WP_REST_Request $request ) {
+	if ( $request->get_param( 'post_as_anonymous' ) && ( $anonymous = get_anonymous_user() ) ) {
+		$params = array_merge( $params, [
+			'comment_author'       => $anonymous->display_name,
+			'comment_author_email' => $anonymous->user_email,
+			'comment_author_url'   => $anonymous->user_url,
+			'user_id'              => $anonymous->ID,
+		] );
+    }
+	return $params;
+}, 10, 2 );
+
+/**
+ * Save original author comment.
+ */
+add_action( 'hamethread_new_comment_inserted', function( $comment_id, array $comment_param, WP_REST_Request $request ) {
+	if ( $request->get_param( 'post_as_anonymous' ) && ( $anonymous = get_anonymous_user() ) ) {
+        update_comment_meta( $comment_id, '_anonymous_author', get_current_user_id() );
+    }
+}, 10, 3 );
+
+/**
+ * Remove anonymous user from subscribers.
+ */
+add_filter( 'hamethread_subscribers', function( $subscribers, $post ) {
+    if ( $anonymous = get_anonymous_user() ) {
+        $subscribers = array_filter( $subscribers, function( $subscriber ) use ( $anonymous ) {
+            return $subscriber != $anonymous->ID;
+        } );
+    }
+    return $subscribers;
 }, 10, 2 );
