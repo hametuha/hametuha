d6774b0ffe60f19775fec3f2bc89f3bbe3378146
fumikito <guy@hametuha.com>
2021-05-06 22:38:59 +0900

    Citeタグをスタイルとして追加

--- src/Hametuha/Commands/Post.php
+++ src/Hametuha/Commands/Post.php
@@ -236,6 +236,7 @@ class Post extends Command {
 			'#<strong>([^<]+)</strong>#u'                       => '<CharStyle:Strong>$1<CharStyle:>',
 			'#<strong class="text-emphasis">([^<]+)</strong>#u' => '<CharStyle:StrongSesami>$1<CharStyle:>',
 			'#<em>([^<]+)</em>#u'                               => '<CharStyle:Emphasis>$1<CharStyle:>',
+			'#<cite>([^<]+)</cite>#u'                           => '<CharStyle:Cite>$1<CharStyle:>',
 			'#<span class="text-emphasis">([^<]+)</span>#u'     => '<CharStyle:EmphasisSesami>$1<CharStyle:>',
 			'#<del>([^<]+)</del>#u'                             => '<CharStyle:Del>$1<CharStyle:>',
 			'#<ruby>([^<]+)<rt>([^>]+)</rt></ruby>#'            => '<cMojiRuby:0><cRuby:1><cRubyString:$2>$1<cMojiRuby:><cRuby:><cRubyString:>',
