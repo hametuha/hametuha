a3e81a2d1d88293dc68df9a2511165ca5eb2fb34
fumikito <guy@hametuha.com>
2025-09-01 00:00:48 +0900

    ファイルフォーマットが間違っていたので修正

--- themes/hametuha/assets/js/src/common.js
+++ themes/hametuha/assets/js/src/common.js
@@ -1292,6 +1292,23 @@ jQuery(document).ready( function ($) {
 
 });
 
+/**
+ * レスポンシブヘルパー
+ * テーブルなどの要素をレスポンシブ対応にする
+ */
+
+jQuery( document ).ready( function ( $ ) {
+	'use strict';
+
+	// .post-content内の直下のテーブルを.table-wrapperでラップ
+	$( '.post-content > table' ).each( function () {
+		// すでにラップされている場合はスキップ
+		if ( ! $( this ).parent().hasClass( 'table-wrapper' ) ) {
+			$( this ).wrap( '<div class="table-wrapper"></div>' );
+		}
+	} );
+} );
+
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
